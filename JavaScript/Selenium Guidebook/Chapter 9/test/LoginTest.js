da5671017e632a05b8ffeabd3f4b5eecced669bb
Christian Bagdon <cbagdon119@gmail.com>
2021-01-19 12:29:53 -0600

    Initial Commit

---
+++ JavaScript/Selenium Guidebook/Chapter 9/test/LoginTest.js
@@ -0,0 +1,30 @@
+const { Builder } = require("selenium-webdriver")
+const assert = require("assert")
+
+const LoginPage = require("../pages/LoginPage")
+
+describe("Test Login Functionality", function() {
+    this.timeout(30000)
+    let driver
+    let page
+
+    beforeEach(async function() {
+        driver = await new Builder().forBrowser("chrome").build()
+        page = new LoginPage(driver)
+        await page.load()
+    })
+
+    afterEach(async function() {
+        await driver.quit()
+    })
+
+    it("with valid credentials", async function() {
+        await page.authenticate("tomsmith", "SuperSecretPassword!")
+        assert(await page.successDisplayed(), "Banner not found!")
+    })
+
+    it("with invalid credentials", async function() {
+        await page.authenticate("tomsmith", "wrong_password")
+        assert(await page.failureDisplayed(), "Banner not found!")
+    })
+})
